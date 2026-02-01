61c8726e159d3d815f0622b99e7081e749a11984
Kishor <kishorbharti010@gmail.com>
2026-02-01 20:50:54 +0530

    color change

--- src/components/Headers/Header.js
+++ src/components/Headers/Header.js
@@ -3,11 +3,16 @@ import { Container } from "reactstrap";
 const Header = () => {
   return (
     <>
-      <div className="header bg-gradient-info pb-8 pt-5 pt-md-8">
+      <div className="header custom-gradient pb-8 pt-5 pt-md-8">
         <Container fluid>
           {/* Header content */}
         </Container>
       </div>
+      <style>{`
+        .custom-gradient {
+          background: linear-gradient(135deg, #1f3c88 0%, #3a5ba0 50%, #2d9cdb 100%);
+        }
+      `}</style>
     </>
   );
 };
