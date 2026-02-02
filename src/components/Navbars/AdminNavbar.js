f0cfb87ca75f3e9d9bfe8ea7eeca98a62e2ca054
Kishor <kishorbharti010@gmail.com>
2026-02-02 19:58:01 +0530

    critical change 2

--- src/components/Navbars/AdminNavbar.js
+++ src/components/Navbars/AdminNavbar.js
@@ -16,42 +16,53 @@ const AdminNavbar = (props) => {
   return (
     <>
       <Navbar className="navbar-top navbar-dark" expand="md" id="navbar-main">
-        <Container fluid>
+        <Container className="d-flex flex-column flex-md-row align-items-center" fluid>
           <Link
-            className="h4 mb-0 text-white text-uppercase d-none d-lg-inline-block"
+            className="h4 mb-0 text-white text-uppercase"
             to="/"
           >
             {props.brandText}
           </Link>
-          <Form className="navbar-search navbar-search-dark form-inline mr-3 d-none d-md-flex ml-lg-auto">
-            <FormGroup className="mb-0">
-              <InputGroup className="input-group-alternative">
-                <InputGroupAddon addonType="prepend">
-                  <InputGroupText>
-                    <i className="fas fa-search" />
-                  </InputGroupText>
-                </InputGroupAddon>
-                <Input placeholder="Search" type="text" />
-              </InputGroup>
-            </FormGroup>
-          </Form>
-          <Nav className="ml-auto align-items-center" navbar>
-            <div className="align-items-center media">
-              <span className="avatar avatar-sm rounded-circle">
-                <img
-                  alt="..."
-                  src={require("../../assets/img/theme/team-4-800x800.jpg")}
-                />
-              </span>
-              <div className="ml-2 d-none d-lg-block media">
-                <span className="mb-0 text-sm font-weight-bold text-white">
+
+          <div className="d-flex align-items-center ml-md-auto mt-2 mt-md-0 w-100 w-md-auto justify-content-between justify-content-md-end">
+            <Form className="navbar-search navbar-search-dark form-inline d-none d-md-flex mr-3">
+              <FormGroup className="mb-0">
+                <InputGroup className="input-group-alternative">
+                  <InputGroupAddon addonType="prepend">
+                    <InputGroupText>
+                      <i className="fas fa-search" />
+                    </InputGroupText>
+                  </InputGroupAddon>
+                  <Input placeholder="Search" type="text" />
+                </InputGroup>
+              </FormGroup>
+            </Form>
+
+            <Nav className="align-items-center" navbar>
+              <div className="profile-widget d-flex align-items-center">
+                <span className="avatar avatar-sm rounded-circle">
+                  <img
+                    alt="..."
+                    src={require("../../assets/img/theme/team-4-800x800.jpg")}
+                  />
+                </span>
+                <span className="ml-2 mb-0 text-sm font-weight-bold text-white d-none d-sm-inline">
                   Kishor Bharti
                 </span>
               </div>
-            </div>
-          </Nav>
+            </Nav>
+          </div>
         </Container>
       </Navbar>
+      <style>{`
+        /* Responsive tweaks for the single profile widget */
+        @media (max-width: 765px), (max-height: 780px) {
+          #navbar-main .profile-widget {
+            margin-left: 0;
+            margin-right: auto;
+          }
+        }
+      `}</style>
     </>
   );
 };
